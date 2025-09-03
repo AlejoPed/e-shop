a47912a9225b0f1627baef6be0741b5eaeb2b7d6
alejoped <lpedreros@unal.edu.co>
2025-09-03 01:13:37 -0500

    commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './modules/order/order.module';
+import { CustomerModule } from './modules/customer/customer.module';
+import { ProductModule } from './modules/product/product.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, CustomerModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
